ac2937b2cecf289abbffe33cf584b5cf96b3b4a6
ddobricki <157130491+ddobricki@users.noreply.github.com>
2024-01-19 14:54:40 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+var myVar = setInterval(function() {
+    myTimer();
+  }, 1000);
+  
+  function myTimer() {
+    var d = new Date();
+    document.getElementById("clock").innerHTML = d.toLocaleTimeString();
+  }
+  
+  window.addEventListener('scroll', handleScroll);
+
+function handleScroll() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;  
+    var scrolled = (winScroll / height) * 100;
+  
+    document.getElementById("progressBar").style.width = scrolled + "%";
+}
